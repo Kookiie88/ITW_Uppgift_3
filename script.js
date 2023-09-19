85c0379eb95bc9e2a7c9c53bc67c94a653a16cf1
Kookiie88 <alexander@jj-media.se>
2023-09-19 12:51:39 +0700

    Filled in all the code for the assignmentw

---
+++ script.js
@@ -0,0 +1,7 @@
+let knapp = document.getElementById("knapp");
+let rubrik = document.getElementById("rubrik");
+
+knapp.addEventListener("click", function() {
+
+    rubrik.textContent = "Goodbye World";
+});
